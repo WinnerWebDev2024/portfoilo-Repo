8f154a73fbedb01ce4b7dcf4449ac5b1bb6f069e
jivan923 <winner.web.dev.2024@gmail.com>
2024-02-02 03:56:43 +0100

    begining of code for protfoli

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log('Hello Word');
+// Menu ham,burgues
+const BOTON = document.querySelector('#icono i'); /* Selecciona en elemento i de la id icono */
+const ENLACES = document.querySelector('#enlaces');
+
+BOTON.addEventListener('click', () =>{
+    ENLACES.classList.toggle('open'); /* añade o quita el class open */
+    setTimeout(() =>{ /* para que espere 600ms y cambia el hamburguer a X */
+        BOTON.classList.toggle('fa-xmark');
+    }, 600);
+});
